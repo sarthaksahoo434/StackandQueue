45a6e40861dbfd320eba6748acf827730ff3d7fd
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-31 17:28:52 +0530

    [sarthak]ADD:Stack

---
+++ StackAndQueueDemo/StackAndQueueDemo/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueueDemo
+{
+    class Node
+    {
+        public Node next;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ StackAndQueueDemo/StackAndQueueDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackAndQueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Queue !");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
